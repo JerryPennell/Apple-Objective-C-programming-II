0928d53201c9f6834d5f6506bf868f84fb69f59a
Jerry Pennell <jwpenn@fullsail.edu>
2013-02-27 19:59:21 -0500

    Added swiper left event
    Swiper event for saving

--- Week4/App3Week3_AOCII/App3Week3_AOCII/addEventView.m
+++ Week4/App3Week3_AOCII/App3Week3_AOCII/addEventView.m
@@ -32,6 +32,37 @@
 
 }
 
+//Adds event to textview on first view
+-(void)onSwipeLeft:(UISwipeGestureRecognizer*)recognizer
+{
+    if (recognizer.direction == UISwipeGestureRecognizerDirectionLeft)
+    {
+        //If text field is blank, throw up an error message letting the user know
+        if([textField.text length] == 0)
+        {
+            errorMessage.textColor = [UIColor redColor];
+            errorMessage.text = @"*Please Enter Event Title";
+            
+        } else if ([textField.text length] > 0) {
+            
+            //Format date
+            NSDateFormatter *f = [[NSDateFormatter alloc] init];
+            [f setDateFormat:@"MMMM d, YYYY 'at' hh:mm a"];
+            NSString *s = [f stringFromDate:datePicker.date];
+            
+            NSString *textString = textField.text;
+            NSString *dateString = s;
+            
+            //Add new info to textview
+            self.textView.text = [[NSString alloc] initWithFormat:@"Event: %@ is scheduled for \n %@ \n\n%@", textString, dateString, self.textView.text];;
+            
+            //Close current view
+            [self dismissViewControllerAnimated:TRUE completion:nil];
+        }
+    }
+}
+
+
 //Close keyboard button
 -(IBAction)closeKeyboard:(id)sender
 {
